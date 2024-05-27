c35d6d19f53038376d666376e2f77b7a3d6e6991
SABBER_H <sabberhasan042@gmail.com>
2024-05-27 23:39:13 +0600

    middleware, middleware namespace, layouts

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Middleware\CheckAdminUser;
+use App\Http\Middleware\CheckMerchantUser;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -11,7 +13,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+       // $middleware->append(CheckAdminRole::class);
+       $middleware->alias(['admin' => CheckAdminUser::class]);
+       $middleware->alias(['merchant' => CheckMerchantUser::class]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-Route::middleware(CheckAdminUser::class)->group(function () {
+Route::middleware('admin')->group(function () {
     Route::get('/try', [UserController::class, 'index']);
     
 });
